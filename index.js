5a4c32a2c3c02c0a715be6fd684f1d0f536d8fb8
Raffi Macaraig <raffi19.macaraig@gmail.com>
2024-04-29 15:11:59 -0700

    number buttons now update display

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 let clearButton = document.querySelector(".clear");
 let result = document.querySelector(".result");
+let numbers = document.querySelectorAll(".number");
 
 clearButton.addEventListener("click", () => {
   result.innerHTML = "0";
+});
+
+numbers.forEach((num) => {
+  num.addEventListener("click", () => {
+    if (result.innerHTML === "0") result.innerHTML = "";
+    result.innerHTML += num.innerHTML;
+  });
 });
